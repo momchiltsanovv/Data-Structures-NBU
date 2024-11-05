c2d054c857b0ab412bcde994621255edc1681a19
Momchil Tsanov <momchil395@gmail.com>
2024-11-05 20:53:56 +0200

    Initial Commit

---
+++ MergeSort/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  MergeSort
+//
+//  Created by Momchil Tsanov on 5.11.24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
