989a1ec3050b6597d5b5238cef4b6d719f8b70fa
Momchil Tsanov <momchil395@gmail.com>
2024-12-14 11:19:21 +0200

    obhojdane durvo

--- DynamicDataStructures/Durveta_narastvane-visochina/main.cpp
+++ DynamicDataStructures/Durveta_narastvane-visochina/main.cpp
@@ -1,4 +1,7 @@
 //
+// Created by Momchil Tsanov on 14.12.24.
+//
+//
 // Created by Momchil Tsanov on 11.12.24.
 //
 #include <iostream>
@@ -6,7 +9,7 @@ using namespace std;
 
 
 struct vuzel {
-    char data;
+    int data;
     vuzel* left;
     vuzel* right;
 };
@@ -26,6 +29,32 @@ vuzel* ibd(int& size) {
 
     return novo;
 }
+
+
+vuzel* find(vuzel* koren, int turseno) { // lokaciq
+    vuzel* loc = koren;
+
+    while (loc != NULL && loc->data != turseno) {
+        cout <<" vuzel: " << loc->data <<endl;
+        if (turseno > loc->data) { // slizame nadolu
+            loc = loc->right;
+            cout << "slqzoh nqdqsno" << endl;
+        }
+        else {
+            loc = loc->left;
+            cout << "sldqzoh nalqvo" << endl;
+        }
+    }
+    if (loc == NULL) {
+        cout << "nqma go" << endl;
+    }
+    else {
+        cout << "namerih" << endl;
+    }
+
+    return loc;
+}
+
 
 void print(vuzel* root, int space) {
     int count = 10;
@@ -86,6 +115,17 @@ int main() {
     cout << endl;
     cout << " постфиксно " << endl;
     postfiksno(root);
+
+    cout << endl;
+
+    cout << "kvo tursish: ";
+    int x;
+    cin >> x;
+
+    vuzel* result = find(root, x);
+    cout << result << endl;
+
+
     free(root);
     return 0;
 }
